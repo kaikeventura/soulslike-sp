9e5c8071be705fc64dcf8e754e43baee6b807d59
kaikeventura <kaike.gnr@hotmail.com>
2025-03-15 15:34:53 -0300

    first commit

---
+++ src/main/java/com/kaikeventura/soulslike_sp/SoulslikeSpApplication.java
@@ -0,0 +1,13 @@
+package com.kaikeventura.soulslike_sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulslikeSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulslikeSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaikeventura/soulslike_sp/SoulslikeSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaikeventura.soulslike_sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulslikeSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
